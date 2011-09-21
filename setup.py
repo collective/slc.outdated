76d8d81fce0303f52c6711856ebb537f69924ccf
Wolfgang Thomas <thomas@syslab.com>
2011-09-21 11:18:17 +0200

    once again, repeating the release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '1.1.1dev'
+version = '1.1'
 shortdesc = "Mark and treat plone content as outdated"
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.txt')).read()
 
