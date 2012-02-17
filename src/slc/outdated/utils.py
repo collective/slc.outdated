c1bdcc23780ef64eb3b0c1490ab204b64a531e1b
JC Brand <jc@opkode.com>
2012-02-17 11:03:43 +0200

    Add utility method to directly check if an object is outdated.

---
+++ src/slc/outdated/utils.py
@@ -0,0 +1,11 @@
+from zope.component import queryMultiAdapter
+from Products.CMFCore.utils import getToolByName
+from plone.indexer.interfaces import IIndexer
+
+def is_outdated(obj):
+    """ Utility method to check if an object is outdated.
+    """
+    catalog = getToolByName(obj, 'portal_catalog')
+    indexer = queryMultiAdapter((obj, catalog), IIndexer, name='outdated')
+    return indexer()
+
